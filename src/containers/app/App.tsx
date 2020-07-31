8ce76e529f72c87c958ebd23d7a1d3007b5d8bb9
rafvanpuyvelde <raf.vanpuyvelde@outlook.com>
2020-07-31 19:37:14 +0200

    React router edit

--- src/containers/app/App.tsx
+++ src/containers/app/App.tsx
@@ -1,56 +1,19 @@
-import React from "react";
-import { Counter } from "../../components/counter/Counter";
 import "./App.css";
+import React from "react";
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import { Counter } from "../../components/counter/Counter";
 
-function App() {
+const history = createBrowserHistory();
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
+    <Router history={history}>
+      <div className="wrapper-app">
         <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
